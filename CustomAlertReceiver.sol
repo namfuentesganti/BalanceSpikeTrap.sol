7f3347508fd52b69a7ca9abb899662b724dfc0d3
namfuentesganti <158285034+namfuentesganti@users.noreply.github.com>
2025-08-09 10:47:50 +0200

    Update CustomAlertReceiver.sol

--- CustomAlertReceiver.sol
+++ CustomAlertReceiver.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.20;
 
 contract CustomAlertReceiver {
-    event SpikeDetected(string message, uint256 currentBalance, uint256 previousBalance);
+    event SpikeDetected(address indexed triggeredBy, string message, uint256 currentBalance, uint256 previousBalance);
 
     function handleSpike(bytes calldata data) external {
         (uint256 current, uint256 previous) = abi.decode(data, (uint256, uint256));
         string memory message = "Balance spike detected!";
-        emit SpikeDetected(message, current, previous);
+        emit SpikeDetected(msg.sender, message, current, previous);
     }
 }
